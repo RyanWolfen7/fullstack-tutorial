3bee15d3ba20f8038665dfb685b779ae1ab99356
Ryan Clark <wolfen7@pop-os.localdomain>
2020-01-30 12:29:43 +0000

    added server listener to index.js

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,3 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
 const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+    console.log(` Ground Controll to Major Tom, we have liftoff at ${url}`)
+})
